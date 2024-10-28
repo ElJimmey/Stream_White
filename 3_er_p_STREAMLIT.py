eecb7eff19da037d3da49d77accbe6aa5afdb7ea
ElJimmey <jvr72832@lasallebajio.edu.mx>
2024-10-28 10:37:42 -0600

    Versión Inicial

---
+++ 3_er_p_STREAMLIT.py
@@ -0,0 +1,12 @@
+#install streamlit pandas numpy plotly
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.set_page_config(
+    page_title = "Monitoreo Industrial" ,
+    layout = "wide"
+)
+
+st.title("Sistema de Monitoreo Industrial")
+st.write("Bienvenido al Sistema de Monitoreo en Tiempo Real")
